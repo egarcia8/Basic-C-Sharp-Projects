5004a7a4eee7f1ea0cca755fc9a1bf0cfcc2d1ce
Evelina Garcia <101986765+egarcia8@users.noreply.github.com>
2022-10-12 10:46:04 -0500

    Edited boolean logic for DUI question and adjusted comparison operators for tickets question

--- CreditApplication/CreditApplication/Program.cs
+++ CreditApplication/CreditApplication/Program.cs
@@ -17,15 +17,15 @@ namespace CreditApplication
             int userAge = Convert.ToInt32(Console.ReadLine());
 
             //print question to screen, declare variable and assign answer to variable userDUI
-            Console.WriteLine("Have you ever had a DUI?");
-            string userDUI = (Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? Please enter true or false.");
+            bool userDUI = Convert.ToBoolean(Console.ReadLine());
 
             //print question to screen, declare variable and assign answer converted to integer to variable userTickets
             Console.WriteLine("How many speeding tickets do you have?");
             int userTickets = Convert.ToInt32(Console.ReadLine());
 
             //declared boolean variable evaluated to true if all three statements are true
-            bool isUserQualified = (userAge > 15 && userDUI == "no" && userTickets < 3);
+            bool isUserQualified = userAge > 15 && userDUI == false && userTickets <= 3;
 
             //print to screen if qualified, print to screen to result of the variable isUserQualified
             Console.WriteLine("Do you qualify?");
