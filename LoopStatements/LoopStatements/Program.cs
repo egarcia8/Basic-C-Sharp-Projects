81e142e353670474c00783c01480f4921477529a
Evelina Garcia <101986765+egarcia8@users.noreply.github.com>
2022-10-13 10:39:24 -0500

    Created a console app that includes a while statement and a do while statement

---
+++ LoopStatements/LoopStatements/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNumber = 0;
+            while (myNumber < 10)
+            {
+                Console.WriteLine(myNumber);
+                myNumber++;
+            }
+           
+
+
+            Console.WriteLine("Guess my favorite number.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int favNum = 15;
+            bool isGuessed = number == favNum;
+           
+
+            do
+            {
+                switch(number)
+                {
+                    case 14:
+                        Console.WriteLine("Close! Try Again");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 15:
+                        Console.WriteLine("That is correct! 15 is my favorite number");
+                        Console.ReadLine();
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("That is not correct. Please try again.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                }
+            }
+            while (!isGuessed);
+            
+        }   
+    }
+}
