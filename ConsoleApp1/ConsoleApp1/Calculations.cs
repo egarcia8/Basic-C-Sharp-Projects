1c9e4ba883811c8949b67c33d4cacb804ac1c250
Evelina Garcia <101986765+egarcia8@users.noreply.github.com>
2022-10-18 16:40:06 -0500

    Created basic console app that utilized method with optional parameter

---
+++ ConsoleApp1/ConsoleApp1/Calculations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExample
+{
+    public class Calculations //created a public class called Calculations
+    {
+        //created a method that takes two integers as parameters, one of them optional.
+        //The method adds two parameters and returns an integer result.
+
+        public static int Total(int num1, int num2 = 5)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExample
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask the user to input two numbers, one at a time.
+            //Let them know they need not enter anything for the second number.
+
+            Console.WriteLine("Please enter the size of your first class."); 
+            int class1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("If you have a second class enter it now.");
+            string checkClass = Console.ReadLine();
+
+            //condition statement to call method passing one or two numbers entered
+            if (String.IsNullOrEmpty(checkClass)) //if did not enter a second number call method to
+                                                  //pass argument and use default 
+            {
+                Calculations students = new Calculations();
+                Console.WriteLine("Your student total is: " + Calculations.Total(class1));
+            }
+            else //if entered second number pass both arguements to call method
+            {
+                int class2 = Convert.ToInt32(checkClass); //convert string to int to pass second argument
+                Console.WriteLine("Your student total is: " + Calculations.Total(class1, class2));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
