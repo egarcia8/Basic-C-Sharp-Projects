e3a84d40049f98808412a0e93be31aafa9d316ac
Evelina Garcia <101986765+egarcia8@users.noreply.github.com>
2022-10-18 10:42:09 -0500

    added comments

--- CallingMethods/CallingMethods/Operations.cs
+++ CallingMethods/CallingMethods/Operations.cs
@@ -6,21 +6,24 @@ using System.Threading.Tasks;
 
 namespace CallingMethods
 {
-    public class Operations
+    public class Operations //created a seperate class to hold methods
     {
-        public static int Add(int userInput)
-        {
-            return userInput + 10;
+        public static int Add(int userInput) //created method with access modifier public(other classes can access it)
+        {                                      //static so as to instantiate it to call it, and it returns an integer value 
+            return userInput + 10;          //the method requires a parameter of an integer provided by the user
+            //the method adds 10 to the user input and returns the value
         }
 
         public static int Multiply(int userInput)
         {
             return userInput * 10;
+            //the method multiplies 10 to the user input and returns the value
         }
 
         public static int Subtract(int userInput)
         {
-            return userInput - 10; 
+            return userInput - 10;
+            //the method subtracts 10 from the user input and returns the value
         }
     }
 }
--- CallingMethods/CallingMethods/Program.cs
+++ CallingMethods/CallingMethods/Program.cs
@@ -9,16 +9,18 @@ namespace CallingMethods
     {
         static void Main(string[] args)
         {
+            //ask user for number to do math opertaions on
             Console.WriteLine("Enter a number greater than 10 to display our math operations");
-            int userInput = Convert.ToInt32(Console.ReadLine());
+            int userInput = Convert.ToInt32(Console.ReadLine()); //declare a integer variable named userInput
+                                                                 //assigning the userInput converted to an integer to it
 
-            if (userInput > 10)
+            if (userInput > 10)//if the number provided is greater than 10 the methods are called
             {
                 Console.WriteLine("Your number plus 10 equals: " + Operations.Add(userInput));
                 Console.WriteLine("Your number times 10 equals: " + Operations.Multiply(userInput));
                 Console.WriteLine("Your number minus 10 equals: " + Operations.Subtract(userInput));
             }
-            else
+            else //if number is 10 or less the message will display on screen
             {
                 Console.WriteLine("That number is not greater than 10.");
             }
