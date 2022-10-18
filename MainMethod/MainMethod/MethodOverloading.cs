4e08c3e78ae59acfdd1a72abddd5abcae9876599
Evelina Garcia <101986765+egarcia8@users.noreply.github.com>
2022-10-18 11:42:55 -0500

    Created example of basic console app utilizing method overloading

---
+++ MainMethod/MainMethod/MethodOverloading.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    //Created a class. In that class, created a method that 
+    //takes in an integer, created a math operation for this integer
+    public class MethodOverloading
+    {
+        public static int Multiply(int number)
+        {
+            return number * 8;
+        }
+
+
+        //Added a second method to the class with the same name that takes in a decimal,
+        //created a different math operation for it, then returns the answer as an integer.
+        public static int Multiply(float number)
+        {
+            int product = (int)(number * 10);
+            return product;
+        }
+
+
+        //Added a third method to the class, with the same name, that takes in a string,
+        //converts it to an integer, does a different math operation to it,
+        //then returns the answer as an integer.
+
+        public static int Multiply(string number)
+        {
+            int product = Convert.ToInt32(number) * 8;
+            return product;
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //instantiate the class and call the one method, passing in an integer.
+            //Display the result to the screen.
+            MethodOverloading example = new MethodOverloading();
+            int answerOne = MethodOverloading.Multiply(6);
+            Console.WriteLine(answerOne);
+
+            //instantiate the class and call the second method,
+            //passing in a decimal. Display the result to the screen.
+            MethodOverloading example2 = new MethodOverloading();
+            int answerTwo = MethodOverloading.Multiply(8.2f);
+            Console.WriteLine(answerTwo);
+
+            //instantiate the class and call the third method, passing in a
+            //string that equates to an integer. Display the result to the screen.
+            MethodOverloading example3 = new MethodOverloading();
+            int answerThree = MethodOverloading.Multiply("32");
+            Console.WriteLine(answerThree);
+
+            Console.ReadLine();
+        }
+        
+    }
+}
