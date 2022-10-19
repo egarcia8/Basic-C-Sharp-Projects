87043d5cf0de2251c0eec1589068812f5de5a70b
Evelina Garcia <101986765+egarcia8@users.noreply.github.com>
2022-10-19 08:46:18 -0500

    Created basic console app focusing on methods, parameters, and arguments

---
+++ MethodPractice/MethodPractice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student studentA = new Student(); //instantiate the class
+
+            Student.testAvg(95, .70f); //Call the method in the class, passing in two numbers
+
+            Student.testAvg(testGrade: 85, weight: .70f); //Call the method in the class,
+                                                          //specifying the parameters by name
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodPractice/MethodPractice/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice
+{
+    public class Student //created public class named Student
+    {
+        //A void method that takes two integers as parameters.
+        //Method does a math operation on the first integer and
+        //displays the second integer to the screen.
+        public static void testAvg(int testGrade, float weight)
+        {
+            float testScores = testGrade * weight;
+            Console.WriteLine("Your weighted test average is " + testScores);
+            Console.WriteLine("Test scores are worth " + weight + " of your class average.");
+
+        }
+    }
+}
