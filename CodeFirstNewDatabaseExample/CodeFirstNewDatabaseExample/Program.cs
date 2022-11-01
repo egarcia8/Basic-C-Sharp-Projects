8c22f7bee84dae1f12b7bca50cc0120f582c64ec
Evelina Garcia <101986765+egarcia8@users.noreply.github.com>
2022-10-31 19:15:53 -0500

    Created a basic Entity Framework Code-First console application

---
+++ CodeFirstNewDatabaseExample/CodeFirstNewDatabaseExample/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace CodeFirstNewDatabaseExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            using (var db = new SchoolContext())
+            {
+                var stud = new Student() { FirstName = "William", LastName = "Darcy" };
+
+                db.Students.Add(stud);
+                db.SaveChanges();
+
+            }
+        }
+
+        public class Student
+        {
+            public int StudentId { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public Course Course { get; set; }
+        }
+
+        public class Course
+        {
+            public int CourseId { get; set; }
+            public string CourseName { get; set; }
+            public string TeacherName { get; set; }
+
+            public List<Student> Students { get; set; }
+        }
+
+        public class SchoolContext : DbContext
+        {
+            public SchoolContext() : base()
+            {
+
+            }
+
+            public DbSet<Student> Students { get; set; }
+            public DbSet<Course> Courses { get; set; }
+        }
+    }
+}
