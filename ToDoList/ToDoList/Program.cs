a0ec33936838f2fcf9f1a8808b86a71ef11087e5
Evelina Garcia <101986765+egarcia8@users.noreply.github.com>
2022-11-01 11:02:35 -0500

    Created a console application of a basic to do list utilizing encapsulation

---
+++ ToDoList/ToDoList/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ToDoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello!\nHere are your daily tasks.\n");
+
+            Task onetask = new Task(1, "Turn on lights.");
+            onetask.StatusReport();
+
+            Task twotask = new Task(2, "Turn on computer.");
+            twotask.StatusReport();
+
+            Task threetask = new Task(3, "Make coffee.");
+            threetask.StatusReport();
+
+            Task fourtask = new Task(4, "Feed fish.");
+            fourtask.StatusReport();
+
+            Task fivetask = new Task(5, "Check emails.");
+            fivetask.StatusReport();
+
+
+            var tasks = new List<Task>();
+            tasks.Add(onetask);
+            tasks.Add(twotask);
+            tasks.Add(threetask);
+            tasks.Add(fourtask);
+            tasks.Add(fivetask);
+
+            int taskCount = tasks.Count;
+
+            while (taskCount > 0)
+            {
+
+                Console.WriteLine($"\nWhich item would you like to mark complete?");
+                var itemId = Convert.ToInt32(Console.ReadLine());
+                foreach (var task in tasks)
+                {
+                    if (itemId == task.Id)
+                    {
+                        task.Status = true;
+                    }
+                    task.StatusReport();
+                }
+
+                taskCount--;
+                Console.WriteLine($"\nYou have {taskCount} items left to do.");
+            }
+            Console.WriteLine($"\nAll items have been complete. The system will exit.");
+        }
+    }
---
+++ ToDoList/ToDoList/Task.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoList
+{
+    class Task
+    {
+        private int _id;
+        private bool _status;
+        private string _item;
+
+        public int Id { get { return _id; } }
+        public string Item { get { return _item; } }
+        public bool Status { get { return _status; } set { _status = value; } }
+
+        public void StatusReport()
+        {
+            Console.WriteLine($"Id: {_id}; Status: {_status}; Item: {_item}");
+        }
+
+        public Task(int id, string item)
+        {
+            _id = id;
+            _item = item;
+            _status = false;
+        }
+    }
+}
